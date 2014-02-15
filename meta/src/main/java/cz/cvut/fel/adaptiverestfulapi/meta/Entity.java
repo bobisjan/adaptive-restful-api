3aa760315995bb663cfff21664e5e08427e81d23
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-02-15 13:14:51 +0100

    [meta] Add attributes and relationships into entity

--- meta/src/main/java/cz/cvut/fel/adaptiverestfulapi/meta/Entity.java
+++ meta/src/main/java/cz/cvut/fel/adaptiverestfulapi/meta/Entity.java
@@ -1,6 +1,8 @@
 
 package cz.cvut.fel.adaptiverestfulapi.meta;
 
+import java.util.Map;
+
 
 /**
  * Represents model entity.
@@ -9,6 +11,9 @@ public class Entity {
 
     private final String name;
     private final Class entityClass;
+
+    private Map<String, Attribute> attributes;
+    private Map<String, Relationship> relationships;
 
     public Entity(String name, Class entityClass) {
         this.name = name;
@@ -21,6 +26,32 @@ public class Entity {
 
     public Class getEntityClass() {
         return this.entityClass;
+    }
+
+    public Attribute attributeForName(String name) {
+        if (name != null) {
+            return this.attributes.get(name);
+        }
+        return null;
+    }
+
+    public Relationship relationshipForName(String name) {
+        if (name != null) {
+            return this.relationships.get(name);
+        }
+        return null;
+    }
+
+    public void addAttribute(Attribute attribute) {
+        if (attribute != null) {
+            this.attributes.put(attribute.getName(), attribute);
+        }
+    }
+
+    public void addRelationship(Relationship relationship) {
+        if (relationship != null) {
+            this.relationships.put(relationship.getName(), relationship);
+        }
     }
 
 }
