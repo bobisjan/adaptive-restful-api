b2e7866e9b34ebb2124317aeb200ac25e8030d6e
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-02-15 11:32:38 +0100

    [meta] Allow to inspect attributes and relationships

--- meta/src/main/java/cz/cvut/fel/adaptiverestfulapi/meta/InspectorListener.java
+++ meta/src/main/java/cz/cvut/fel/adaptiverestfulapi/meta/InspectorListener.java
@@ -1,9 +1,16 @@
 
 package cz.cvut.fel.adaptiverestfulapi.meta;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 
 public interface InspectorListener {
 
     public Entity inspect(Class clazz);
+
+    public Attribute inspectAttribute(Field field, Method getter, Method setter);
+
+    public Relationship inspectRelationship(Field field, Method getter, Method setter);
 
 }
--- meta/src/test/java/cz/cvut/fel/adaptiverestfulapi/meta/Listener.java
+++ meta/src/test/java/cz/cvut/fel/adaptiverestfulapi/meta/Listener.java
@@ -4,6 +4,9 @@ package cz.cvut.fel.adaptiverestfulapi.meta;
 import cz.cvut.fel.adaptiverestfulapi.meta.Entity;
 import cz.cvut.fel.adaptiverestfulapi.meta.InspectorListener;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 
 public class Listener implements InspectorListener {
 
@@ -12,6 +15,27 @@ public class Listener implements InspectorListener {
         if (clazz.getSimpleName().equalsIgnoreCase("Person")) {
             return new Entity(clazz.getSimpleName(), clazz);
         }
+        return null;
+    }
+
+    @Override
+    public Attribute inspectAttribute(Field field, Method getter, Method setter) {
+        if (field != null) {
+            // TODO implement
+            return new Attribute(field.getName());
+
+        }
+        // TODO virtual getter, setter
+        return null;
+    }
+
+    @Override
+    public Relationship inspectRelationship(Field field, Method getter, Method setter) {
+        if (field != null) {
+            // TODO implement
+            return new Relationship(field.getName());
+        }
+        // TODO virtual getter, setter
         return null;
     }
 }
