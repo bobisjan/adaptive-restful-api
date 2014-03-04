fdf7e85717110583c1a82369c28f3180d6efd06b
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-03-04 18:28:56 +0100

    [example] Make model listener generic

--- example/src/main/java/cz/cvut/fel/adaptiverestfulapi/example/ModelListener.java
+++ example/src/main/java/cz/cvut/fel/adaptiverestfulapi/example/ModelListener.java
@@ -10,6 +10,9 @@ import cz.cvut.fel.adaptiverestfulapi.meta.reflection.Triplet;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.Set;
 
 
@@ -22,45 +25,16 @@ public class ModelListener implements ModelInspectionListener {
 
     @Override
     public Property property(Triplet<Field, Method, Method> triplet, Entity entity, Set<Entity> entities) {
-        // TODO make property inspector generic and then move it to the core
-
-        Field field = triplet.a;
         Method getter = triplet.b;
         Method setter = triplet.c;
 
-        if (field != null) {
-            if (field.getName().equalsIgnoreCase("project")) {
-                return new Relationship(this.propertyName(triplet, entity), getter, setter, field.getDeclaringClass().getPackage().getName() + ".Project");
+        String name = this.propertyName(triplet, entity);
+        Entity targetEntity = this.targetEntity(triplet, entity, entities);
 
-            } else if (field.getName().equalsIgnoreCase("issues")) {
-                return new Relationship(this.propertyName(triplet, entity), getter, setter, field.getDeclaringClass().getPackage().getName() + ".Issue");
-
-            } else {
-                return new Attribute(this.propertyName(triplet, entity), getter, setter);
-            }
+        if (targetEntity != null) {
+            return new Relationship(name, getter, setter, targetEntity.getName());
         }
-
-        if (getter != null && setter != null) {
-            if (getter.getName().equalsIgnoreCase("isStarted") && setter.getName().equalsIgnoreCase("setStarted")) {
-                return new Attribute(this.propertyName(triplet, entity), getter, setter);
-            }
-            return null;
-
-        } else if (getter != null) {
-            if (getter.getName().equalsIgnoreCase("getLocalizedDescription")) {
-                return new Attribute(this.propertyName(triplet, entity), getter, null);
-            }
-            return null;
-
-        } else if (setter != null) {
-            if (setter.getName().equalsIgnoreCase("setLowerCasedName")) {
-                return new Attribute(this.propertyName(triplet, entity), null, setter);
-            }
-            return null;
-
-        } else {
-            return null;
-        }
+        return new Attribute(name, getter, setter);
     }
 
     protected String entityName(Class clazz) {
@@ -82,11 +56,69 @@ public class ModelListener implements ModelInspectionListener {
             return entity.getName() + "." + getter.getName().substring("is".length());
 
         } else if (setter != null && setter.getName().startsWith("set"))  {
-            return entity.getName() + "." + getter.getName().substring("set".length());
+            return entity.getName() + "." + setter.getName().substring("set".length());
 
         } else {
             return null;
         }
+    }
+
+    protected Entity targetEntity(Triplet<Field, Method, Method> triplet, Entity entity, Set<Entity> entities) {
+        Class<?> target = this.targetClass(triplet);
+
+        for (Entity e : entities) {
+            if (e.getEntityClass().equals(target)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    // TODO Add check for JPA relationship annotation
+    // TODO Refactor
+    protected Class<?> targetClass(Triplet<Field, Method, Method> triplet) {
+        Class<?> target = null;
+
+        if (triplet.a != null) {
+            target = triplet.a.getType();
+
+            if (Collection.class.isAssignableFrom(target)) {
+                Type type = triplet.a.getGenericType();
+
+                if (type instanceof ParameterizedType) {
+                    ParameterizedType pType = (ParameterizedType)type;
+                    Type[] arr = pType.getActualTypeArguments();
+                    target = (Class<?>)arr[0];
+                }
+            }
+
+        } else if (triplet.b != null) {
+            target = triplet.b.getReturnType();
+
+            if (Collection.class.isAssignableFrom(target)) {
+                Type type = triplet.b.getGenericReturnType();
+
+                if (type instanceof ParameterizedType) {
+                    ParameterizedType pType = (ParameterizedType)type;
+                    Type[] arr = pType.getActualTypeArguments();
+                    target = (Class<?>)arr[0];
+                }
+            }
+
+        } else if (triplet.c != null) {
+            target = triplet.c.getParameterTypes()[0];
+
+            if (Collection.class.isAssignableFrom(target)) {
+                Type type = triplet.c.getGenericParameterTypes()[0];
+
+                if (type instanceof ParameterizedType) {
+                    ParameterizedType pType = (ParameterizedType)type;
+                    Type[] arr = pType.getActualTypeArguments();
+                    target = (Class<?>)arr[0];
+                }
+            }
+        }
+        return target;
     }
 
 }
