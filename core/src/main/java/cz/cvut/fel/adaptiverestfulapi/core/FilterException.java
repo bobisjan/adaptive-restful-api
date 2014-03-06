c16a38c13d5cf548b1ebc5e96f21ec44963ee7cc
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-03-06 08:49:56 +0100

    [core] Add error code into the filter exception

--- core/src/main/java/cz/cvut/fel/adaptiverestfulapi/core/FilterException.java
+++ core/src/main/java/cz/cvut/fel/adaptiverestfulapi/core/FilterException.java
@@ -4,16 +4,28 @@ package cz.cvut.fel.adaptiverestfulapi.core;
 
 public class FilterException extends Exception {
 
+    protected int code;
+
     public FilterException() {
         this("Something gets broken during the filter process.");
     }
 
     public FilterException(String message) {
+        this(message, 500);
+    }
+
+    public FilterException(String message, int code) {
         super(message);
+        this.code = code;
     }
 
     public FilterException(Exception e) {
         super(e);
+        this.code = 500;
+    }
+
+    public int getCode() {
+        return this.code;
     }
 
 }
