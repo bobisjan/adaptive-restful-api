cc85e9190df51ae6a1475c0bafc4e8d8dd221220
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-03-07 09:58:58 +0100

    [security] Set default authentication exception

--- security/src/main/java/cz/cvut/fel/adaptiverestfulapi/security/AuthenticationException.java
+++ security/src/main/java/cz/cvut/fel/adaptiverestfulapi/security/AuthenticationException.java
@@ -2,8 +2,13 @@
 package cz.cvut.fel.adaptiverestfulapi.security;
 
 import cz.cvut.fel.adaptiverestfulapi.core.FilterException;
+import cz.cvut.fel.adaptiverestfulapi.core.HttpStatus;
 
 
 public class AuthenticationException extends FilterException {
+
+    public AuthenticationException() {
+        super(HttpStatus.S_401);
+    }
 
 }
