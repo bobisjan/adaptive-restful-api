f1c0775f9bcfcd659e7f9b55a0efd030a3fcb2d7
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-03-07 13:20:41 +0100

    [security] Fix basic authentication exception

--- security/src/main/java/cz/cvut/fel/adaptiverestfulapi/security/BasicAuthenticationException.java
+++ security/src/main/java/cz/cvut/fel/adaptiverestfulapi/security/BasicAuthenticationException.java
@@ -4,10 +4,14 @@ package cz.cvut.fel.adaptiverestfulapi.security;
 
 public class BasicAuthenticationException extends AuthenticationException {
 
+    public BasicAuthenticationException() {
+        this(null);
+    }
+
     public BasicAuthenticationException(String realm) {
         super();
         // TODO Replace string with constant
-        this.headers.put("WWW-Authorization", "Basic realm=\"" + realm + "\"");
+        this.headers.put("WWW-Authenticate", "Basic" + (realm != null ? " realm=\"" + realm + "\"" : ""));
     }
 
 }
