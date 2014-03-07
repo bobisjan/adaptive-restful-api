779f5715bac37e6170503780eec5d9ec22ed19d6
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-03-07 12:20:40 +0100

    [security] Make basic authentication more generic

--- security/src/main/java/cz/cvut/fel/adaptiverestfulapi/security/BasicAuthentication.java
+++ security/src/main/java/cz/cvut/fel/adaptiverestfulapi/security/BasicAuthentication.java
@@ -4,36 +4,30 @@ import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import cz.cvut.fel.adaptiverestfulapi.core.HttpContext;
 
 import java.util.AbstractMap;
-import java.util.HashMap;
 import java.util.Map;
 
 
 /**
- * HTTP basic authentication.
+ * Abstract HTTP basic authentication.
  */
-public class BasicAuthentication extends Authentication {
-
-    private Map<String, String> users;
-
-    public BasicAuthentication() {
-        this(new HashMap<String, String>());
-    }
-
-    public BasicAuthentication(Map<String, String> users) {
-        this.users = new HashMap<>();
-        for (Map.Entry<String, String> user : users.entrySet()) {
-            this.users.put(user.getKey(), user.getValue());
-        }
-    }
+public abstract class BasicAuthentication extends Authentication {
 
     @Override
-    protected void authenticate(HttpContext httpContext) throws AuthenticationException {
-        Map.Entry user = this.user(httpContext);
+    protected final void authenticate(HttpContext httpContext) throws AuthenticationException {
+        Map.Entry<String, String> user = this.user(httpContext);
 
-        if (!this.users.containsKey(user.getKey()) || !this.users.get(user.getKey()).equals(user.getValue())) {
+        if (!this.isAuthenticated(user.getKey(), user.getValue())) {
             throw new AuthenticationException();
         }
     }
+
+    /**
+     * Returns whether the current user is authenticated.
+     * @param username The user's name.
+     * @param password The user's password.
+     * @return
+     */
+    protected abstract boolean isAuthenticated(String username, String password);
 
     private Map.Entry<String, String> user(HttpContext httpContext) throws AuthenticationException {
         // TODO Get rid off the string, use constant
