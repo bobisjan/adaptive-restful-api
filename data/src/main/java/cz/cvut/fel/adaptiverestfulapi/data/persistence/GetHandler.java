8457c18cb3928ccee5285b5695410a955e3098fb
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-03-08 15:37:14 +0100

    [data] Add persistence handler for GET

---
+++ data/src/main/java/cz/cvut/fel/adaptiverestfulapi/data/persistence/GetHandler.java
@@ -0,0 +1,55 @@
+
+package cz.cvut.fel.adaptiverestfulapi.data.persistence;
+
+import cz.cvut.fel.adaptiverestfulapi.core.HttpContext;
+import cz.cvut.fel.adaptiverestfulapi.core.HttpRouter;
+import cz.cvut.fel.adaptiverestfulapi.data.DataException;
+import cz.cvut.fel.adaptiverestfulapi.meta.configuration.Configuration;
+import cz.cvut.fel.adaptiverestfulapi.meta.model.Entity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+
+public class GetHandler extends cz.cvut.fel.adaptiverestfulapi.data.GetHandler {
+
+    protected EntityManager manager;
+
+    public GetHandler(EntityManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    protected HttpContext get(Entity entity, HttpContext context, Configuration configuration) throws DataException {
+        HttpRouter router = context.getRouter();
+        String identifier = router.getIdentifier();
+        Object result = null;
+
+        if (identifier != null) {
+            result = this.find(entity, identifier);
+
+        } else {
+            result = this.findAll(entity);
+        }
+
+        context.setContent(result);
+        return context;
+    }
+
+    protected Object find(Entity entity, Object identifier) {
+        return this.manager.find(entity.getEntityClass(), identifier);
+    }
+
+    protected List findAll(Entity entity) {
+        CriteriaBuilder qb = this.manager.getCriteriaBuilder();
+        CriteriaQuery criteria = qb.createQuery(entity.getEntityClass());
+        Root root = criteria.from(entity.getEntityClass());
+        TypedQuery query = this.manager.createQuery(criteria);
+        return query.getResultList();
+    }
+
+}
