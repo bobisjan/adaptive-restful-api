9e95e063d972e8ef2d5428104b4089d235a3b36d
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-03-08 15:41:04 +0100

    [data] Add persistence handler for POST

---
+++ data/src/main/java/cz/cvut/fel/adaptiverestfulapi/data/persistence/PostHandler.java
@@ -0,0 +1,33 @@
+
+package cz.cvut.fel.adaptiverestfulapi.data.persistence;
+
+import cz.cvut.fel.adaptiverestfulapi.core.HttpContext;
+import cz.cvut.fel.adaptiverestfulapi.data.DataException;
+import cz.cvut.fel.adaptiverestfulapi.meta.configuration.Configuration;
+import cz.cvut.fel.adaptiverestfulapi.meta.model.Entity;
+
+import javax.persistence.EntityManager;
+
+
+public class PostHandler extends cz.cvut.fel.adaptiverestfulapi.data.PostHandler {
+
+    protected EntityManager manager;
+
+    public PostHandler(EntityManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    protected HttpContext post(Entity entity, HttpContext context, Configuration configuration) throws DataException {
+        Object object = context.getContent();
+        Object result = this.create(entity, object);
+        context.setContent(result);
+        return context;
+    }
+
+    protected Object create(Entity entity, Object object) {
+        this.manager.persist(object);
+        return object;
+    }
+
+}
