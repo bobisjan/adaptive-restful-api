173a728556fa7ee1b0f1f79ca95ce2763e9562d2
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-03-11 11:09:13 +0100

    [data] Update POST handler implementation

--- data/src/main/java/cz/cvut/fel/adaptiverestfulapi/data/persistence/PostHandler.java
+++ data/src/main/java/cz/cvut/fel/adaptiverestfulapi/data/persistence/PostHandler.java
@@ -20,6 +20,11 @@ public class PostHandler extends cz.cvut.fel.adaptiverestfulapi.data.PostHandler
     @Override
     protected HttpContext post(Entity entity, HttpContext context, Configuration configuration) throws DataException {
         Object object = context.getContent();
+
+        if (object == null) {
+            throw new DataException("Object for POST is null.");
+        }
+
         Object result = this.create(entity, object);
         context.setContent(result);
         return context;
@@ -27,7 +32,9 @@ public class PostHandler extends cz.cvut.fel.adaptiverestfulapi.data.PostHandler
 
     protected Object create(Entity entity, Object object) {
         this.manager.getTransaction().begin();
-        this.manager.persist(object);
+        this.manager.merge(object);
+        this.manager.flush();
+        this.manager.clear();
         this.manager.getTransaction().commit();
         return object;
     }
