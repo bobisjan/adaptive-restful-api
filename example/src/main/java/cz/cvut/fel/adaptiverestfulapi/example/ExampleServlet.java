46d48bb053a288981d4c9e976aba3db3da5256af
Jan Bobisud <bobisjan@fel.cvut.cz>
2014-03-11 17:11:40 +0100

    [example] Use If-Modified-Since cache

--- example/src/main/java/cz/cvut/fel/adaptiverestfulapi/example/ExampleServlet.java
+++ example/src/main/java/cz/cvut/fel/adaptiverestfulapi/example/ExampleServlet.java
@@ -1,6 +1,7 @@
 
 package cz.cvut.fel.adaptiverestfulapi.example;
 
+import cz.cvut.fel.adaptiverestfulapi.caching.IfModifiedSinceCache;
 import cz.cvut.fel.adaptiverestfulapi.core.Filter;
 import cz.cvut.fel.adaptiverestfulapi.data.Dispatcher;
 import cz.cvut.fel.adaptiverestfulapi.serialization.Resolver;
@@ -11,10 +12,11 @@ public class ExampleServlet extends FilteredServlet {
 
     public ExampleServlet() {
         Filter auth = new ExampleAuthentication();
+        Filter cache = new IfModifiedSinceCache();
         Filter serializer = new Resolver();
         Filter data = new Dispatcher();
 
-        auth.setNext(serializer.setNext(data));
+        auth.setNext(cache.setNext(serializer.setNext(data)));
         this.filter = auth;
     }
 
